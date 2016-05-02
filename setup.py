1397e0b4fde381066bc4a99d5a263097724e80aa
Rodrigo F. Díaz <rodrigo.diaz@unige.ch>
2016-05-02 23:10:08 +0200

    Moving files to gp directory for setup script.

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='gp',
+      version='1.0',
+      author='Rodrigo F. Diaz',
+      author_email='rodrigo.diaz@unige.ch',
+      packages=['gp'],
+      )
